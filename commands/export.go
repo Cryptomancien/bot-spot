248f8a1867f2d0040a162570f23154d350394580
Cryptomancien <resethread@gmail.com>
2025-06-17 14:35:42 +0200

    add export json

--- commands/export.go
+++ commands/export.go
@@ -4,23 +4,28 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/fatih/color"
+	"github.com/ostafen/clover"
+	"log"
 	"main/database"
 	"os"
 	"path/filepath"
 	"time"
 )
 
-func Export() {
-	// Format current timestamp like Ruby (e.g. "2025-05-13 14:30:00")
-	timestamp := time.Now().Format("2006-01-02 15-04-05")
-
-	// Create "exports" folder if it doesn't exist
+func fileNamePrefix() string {
+	// Create an "exports" folder if it doesn't exist
 	if err := os.MkdirAll("exports", os.ModePerm); err != nil {
 		panic(fmt.Errorf("failed to create exports folder: %w", err))
 	}
 
-	// Create CSV file in the "exports" folder
-	fileName := fmt.Sprintf("exports/%s.csv", timestamp)
+	timestamp := time.Now().Format("2006-01-02 15-04-05")
+
+	filePrefix := fmt.Sprintf("exports/%s", timestamp)
+	return filePrefix
+}
+
+func ToCSV() {
+	fileName := fileNamePrefix() + ".csv"
 
 	color.Yellow("Export data to CSV file: " + fileName)
 
@@ -70,4 +75,27 @@ func Export() {
 		}
 	}
 	color.Green("Successfully Export data to CSV file: " + fileName)
+}
+
+func ToJSON() {
+	fileName := fileNamePrefix() + ".json"
+
+	db := database.GetDB()
+	defer func(db *clover.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Fatalf("can't close database %s", err)
+		}
+	}(db)
+
+	err := db.ExportCollection(database.CollectionName, fileName)
+	if err != nil {
+		log.Fatal("Can't export collection to JSON file: " + fileName)
+	}
+	color.Green("Successfully Export data to JSON file: " + fileName)
+}
+
+func Export() {
+	ToCSV()
+	ToJSON()
 }
---
+++ commands/export_test.go
@@ -0,0 +1,9 @@
+package commands
+
+import (
+	"testing"
+)
+
+func TestExport(t *testing.T) {
+	Export()
+}
