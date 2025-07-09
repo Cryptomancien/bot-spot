cb0b48d6f3b7fa4baaf79b613747047c00b14661
Cryptomancien <resethread@gmail.com>
2025-07-09 10:17:13 +0200

    add test cancel

--- commands/cancel_test.go
+++ commands/cancel_test.go
@@ -1,1 +1,28 @@
 package commands
+
+import (
+	"github.com/joho/godotenv"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestCancel(t *testing.T) {
+	err := godotenv.Load("../bot.conf")
+	if err != nil {
+		t.Fatalf("Error loading ../bot.conf: %v", err)
+	}
+
+	orderID := os.Getenv("ORDER_ID")
+	if orderID == "" {
+		t.Fatal("Missing environment variable: ORDER_ID")
+	}
+
+	client := GetClientByExchange()
+	client.CheckConnection()
+
+	cancelOrder, err := client.CancelOrder(orderID)
+	if err != nil {
+		log.Fatal("Cannot cancel order: ", string(cancelOrder))
+	}
+}
