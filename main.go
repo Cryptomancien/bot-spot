abac3ab4afa56aebe0e569d2b210703f98292511
Cryptomancien <resethread@gmail.com>
2025-08-05 13:09:57 +0200

    Restore DB from JSON backup

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func menu() {
 	fmt.Println("--auto			-a		Mode auto")
 	fmt.Println("--clear 		-cl		Clear range (start end) - Example: -cl 12 36")
 	fmt.Println("--export		-e		Export CSV file")
+	fmt.Println("--restore		-r		Restore database from JSON file")
 	fmt.Println("")
 }
 
@@ -50,20 +51,22 @@ func main() {
 	args := os.Args[1:]
 
 	actions := map[string]func(){
-		"--new":    commands.New,
-		"-n":       commands.New,
-		"--update": commands.Update,
-		"-u":       commands.Update,
-		"--server": commands.Server,
-		"-s":       commands.Server,
-		"--cancel": commands.Cancel,
-		"-c":       commands.Cancel,
-		"--auto":   commands.Auto,
-		"-a":       commands.Auto,
-		"--clear":  commands.Clear,
-		"-cl":      commands.Clear,
-		"--export": func() { commands.Export(true) },
-		"-e":       func() { commands.Export(true) },
+		"--new":     commands.New,
+		"-n":        commands.New,
+		"--update":  commands.Update,
+		"-u":        commands.Update,
+		"--server":  commands.Server,
+		"-s":        commands.Server,
+		"--cancel":  commands.Cancel,
+		"-c":        commands.Cancel,
+		"--auto":    commands.Auto,
+		"-a":        commands.Auto,
+		"--clear":   commands.Clear,
+		"-cl":       commands.Clear,
+		"--export":  func() { commands.Export(true) },
+		"-e":        func() { commands.Export(true) },
+		"--restore": commands.Restore,
+		"-r":        commands.Restore,
 	}
 
 	for key, action := range actions {
